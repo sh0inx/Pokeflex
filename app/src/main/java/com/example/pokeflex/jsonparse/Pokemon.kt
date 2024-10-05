d9ab5e0b835e439852c0f781e1769c9154ec6f96
maloudnee <maloudnee62@yahoo.com>
2024-10-05 11:09:33 -0400

    Create Pokemon.kt
    parse json data

---
+++ app/src/main/java/com/example/pokeflex/jsonparse/Pokemon.kt
@@ -0,0 +1,25 @@
+package com.example.pokeflex.jsonparse
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import java.io.File
+
+@Serializable
+data class Pokemon(
+    val name: String,
+    val type: String,
+    val id: Int,
+    val sprite: String
+)
+
+@Serializable
+data class Pokedex(
+    val pokemon: List<Pokemon>
+)
+
+fun readJsonFile(filepath: String): Pokedex{
+    val jsonString = File(filepath).readText() //Reading contents of the file as a string
+    val json = Json { ignoreUnknownKeys = true} //
+    return json.decodeFromString<Pokedex>(jsonString);
+}
+
+
