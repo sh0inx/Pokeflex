0c005aa3c1b815de29da9fe60a44d73460f434dc
Shyanne <randomgirlisweird@gmail.com>
2024-10-05 10:53:31 -0400

    fixed gradle for cardview (oopsies)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation(androidx.cardview:cardview:1.0.0)
+    implementation("androidx.cardview:cardview:1.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
